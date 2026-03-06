6dc03ed60db689dacc7b73180d0f6fd371415054
gustavomgama <gutomgama@gmail.com>
2026-03-06 14:54:35 -0300

    Updates seed file with relevant level 1 test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+MenuItem.destroy_all
+Menu.destroy_all
+
+lunch_menu = Menu.create!(
+  name: "Lunch Menu",
+)
+
+dinner_menu = Menu.create!(
+  name: "Dinner Menu",
+)
+
+MenuItem.create!([
+  { name: "Burger", description: "A classic beef burger with cheese.", price: 9.00, menu: lunch_menu },
+  { name: "Small Salad", description: "Fresh greens with vinaigrette.", price: 5.00, menu: lunch_menu },
+  { name: "Chicken Wings", description: "6 pieces of buffalo wings.", price: 9.00, menu: lunch_menu }
+])
+
+MenuItem.create!([
+  { name: "Mega Burger", description: "Double patty beef burger with bacon.", price: 15.00, menu: dinner_menu },
+  { name: "Large Salad", description: "Healthy greens with grilled chicken.", price: 8.00, menu: dinner_menu },
+  { name: "Lobster Mac & Cheese", description: "Decadent mac and cheese with lobster chunks.", price: 31.00, menu: dinner_menu }
+])
+
+puts "Done."
