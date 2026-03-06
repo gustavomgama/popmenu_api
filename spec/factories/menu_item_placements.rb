4e83257b85fc9e6565394780680c0121f76716c4
gustavomgama <gutomgama@gmail.com>
2026-03-06 19:10:10 -0300

    Updates Factory Bot test data

--- spec/factories/menu_item_placements.rb
+++ spec/factories/menu_item_placements.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :menu_item_placement do
-    menu { nil }
-    menu_item { nil }
+    menu
+    menu_item
     price { "9.99" }
   end
 end
--- spec/factories/menu_items.rb
+++ spec/factories/menu_items.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
   factory :menu_item do
     name { "Burger" }
     description { "Beef with cheese" }
-    price { "9.99" }
-    menu
   end
 end
