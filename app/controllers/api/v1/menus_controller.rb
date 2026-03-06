98ccf036b762f666db01341546808019a87edef1
gustavomgama <gutomgama@gmail.com>
2026-03-06 19:09:10 -0300

    Updates MenusController to reflect updated associations

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::MenusController < ApplicationController
   def index
-    menus = Menu.includes(:menu_items).all
+    menus = Menu.includes(menu_item_placements: :menu_item).all
 
-    render json: menus.as_json(include: :menu_items)
+    render json: menus.as_json(include: { menu_item_placements: { include: :menu_item } })
   end
 
   def show
-    menu = Menu.includes(:menu_items).find(params[:id])
+    menu = Menu.includes(menu_item_placements: :menu_item).find(params[:id])
 
-    render json: menu.as_json(include: :menu_items)
+    render json: menu.as_json(include: { menu_item_placements: { include: :menu_item } })
 
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Menu not found" }, status: :not_found
