53cce7b76ca8ba64e39cfbda5892c352b41fd663
gustavomgama <gutomgama@gmail.com>
2026-03-07 17:30:35 -0300

    Updates controller to use strong params

--- app/controllers/api/v1/ingestions_controller.rb
+++ app/controllers/api/v1/ingestions_controller.rb
@@ -1,8 +1,26 @@
 class Api::V1::IngestionsController < ApplicationController
   def create
-    service = Import::RestaurantDataService.new(params.to_unsafe_h)
+    if params[:restaurants].present?
+      ingestion_params = params.permit(
+        restaurants: [
+          :name,
+          menus: [
+            :name,
+            :description,
+            dishes: [ :name, :price, :description ],
+            menu_items: [ :name, :price, :description ]
+          ]
+        ]
+      ).to_h
+    else
+      ingestion_params = params.to_unsafe_h.slice("restaurants")
+    end
+
+    service = Import::RestaurantDataService.new(ingestion_params)
     results = service.call
 
-    render json: { results: }
+    status = results[:status] == "error" ? :unprocessable_entity : :ok
+
+    render json: results, status:
   end
 end
