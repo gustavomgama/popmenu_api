3aceda53c216d7c7a5914a7fecee172f76b8a18b
gustavomgama <gutomgama@gmail.com>
2026-03-07 17:29:33 -0300

    Improves error handling and logging for the import service

--- app/services/import/restaurant_data_service.rb
+++ app/services/import/restaurant_data_service.rb
@@ -10,40 +10,61 @@ module Import
         process_restaurants
       end
 
-      @results
+      summary_result(status: @results.any? { |r| r[:status] == :error } ? "partial_success" : "success")
+    rescue => e
+      summary_result(status: "error", message: "Fatal error: #{e.message}")
     end
 
     private
 
+    def summary_result(status:, message: nil)
+      {
+        status:,
+        message:,
+        total_processed: @results.count,
+        success_count: @results.count { |r| r[:status] == :success },
+        error_count: @results.count { |r| r[:status] == :error },
+        results: @results
+      }.compact
+    end
+
     def process_restaurants
+      return if @data["restaurants"].blank?
+
       @data["restaurants"].each do |restaurant_data|
-        restaurant = Restaurant.find_or_create_by!(name: restaurant_data["name"])
+        restaurant = Restaurant.find_or_initialize_by(name: restaurant_data["name"])
+        restaurant.save!
 
         process_menus(restaurant, restaurant_data["menus"])
       end
     end
 
     def process_menus(restaurant, menus_data)
+      return if menus_data.blank?
+
       menus_data.each do |menu_data|
-        menu = restaurant.menus.find_or_create_by!(name: menu_data["name"])
+        menu = restaurant.menus.find_or_initialize_by(name: menu_data["name"])
+        menu.save!
 
         items = menu_data["dishes"] || menu_data["menu_items"]
-
         process_items(menu, items)
       end
     end
 
     def process_items(menu, items_data)
+      return if items_data.blank?
+
       items_data.each do |item_data|
         begin
-          item = MenuItem.find_or_create_by!(name: item_data["name"]) do |i|
-            i.description = item_data["description"]
-          end
+          item = MenuItem.find_or_initialize_by(name: item_data["name"])
+          item.description = item_data["description"]
+          item.save!
 
-          placement = menu.menu_item_placements.find_or_create_by!(
-            menu_item: item,
-            price: item_data["price"]
+          placement = menu.menu_item_placements.find_or_initialize_by(
+            menu_item: item
           )
+          placement.price = item_data["price"]
+          placement.save!
 
           @results << {
             restaurant: menu.restaurant.name,
